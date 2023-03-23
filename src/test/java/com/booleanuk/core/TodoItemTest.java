a9d2bb28b75e1316636660916dbeaa3a3c850bfc
Dave Ames <david.john.ames@gmail.com>
2023-03-23 17:03:18 +0000

    Added test to check that getStatus returns the value we used in the Constructor for the Status.

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
-
+    @Test
+    public void checkGetStatusCorrectlyReturnsStatus() {
+        TodoItem todo = new TodoItem("Tidy up", "Tidy up the living room", "Incomplete");
+        Assertions.assertEquals("Incomplete", todo.getStatus());
+    }
 }
